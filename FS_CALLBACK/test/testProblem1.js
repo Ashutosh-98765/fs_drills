6f24ef7e60ab88d27aeb923925456502c1dab325
Ashutosh-98765 <asshu65656@gmail.com>
2024-10-02 18:22:55 +0530

    refactored functions in test files

--- FS_CALLBACK/test/testProblem1.js
+++ FS_CALLBACK/test/testProblem1.js
@@ -1,29 +1,29 @@
-const {create_directory , create_files , delete_files} = require('../problem1.js');
+const { create_directory, create_files, delete_files } = require('../problem1.js');
 
-callBackHell = () => {create_directory((err , msg) => 
-    {
+callBackHell = () => {
+    create_directory((err, msg) => {
         if (err) {
             return console.log(err);
         } else {
             console.log(msg);
         }
 
-        create_files(3,(err, msg) => {
+        create_files(3, (err, msg) => {
             if (err) {
-                return console.log(err);  
+                return console.log(err);
             }
-            else{
+            else {
                 console.log(msg);
             }
 
-            delete_files((err , msg) => {
-                if(err){
+            delete_files((err, msg) => {
+                if (err) {
                     return console.log(err);
                 }
-                else{
+                else {
                     console.log(msg);
                 }
-            });    
+            });
         });
     })
 }
--- FS_CALLBACK/test/testProblem2.js
+++ FS_CALLBACK/test/testProblem2.js
@@ -1,34 +1,34 @@
-const {readFile , createFile , upperCase , lowerCase , sorted , delete_files} = require('../problem2.js');
+const { readFile, createFile, upperCase, lowerCase, sorted, delete_files } = require('../problem2.js');
 
-callBackHell = () => {readFile((err , file_data) => 
-    {
-        if(err){
+callBackHell = () => {
+    readFile((err, file_data) => {
+        if (err) {
             return console.log(err);
         }
         console.log(file_data);
 
-        createFile((err , filename_path) => {
-            if(err){
+        createFile((err, filename_path) => {
+            if (err) {
                 return console.log(err);
             }
 
-            upperCase(file_data , (err , upper_data) => {
-                if(err){
+            upperCase(file_data, (err, upper_data) => {
+                if (err) {
                     return console.log(err);
                 }
 
-                lowerCase(upper_data , (err , lower_data) => {
-                    if(err){
+                lowerCase(upper_data, (err, lower_data) => {
+                    if (err) {
                         return console.log(err);
                     }
-                    
-                    sorted(lower_data , (err) => {
-                        if(err){
+
+                    sorted(lower_data, (err) => {
+                        if (err) {
                             return console.log(err);
                         }
-                            
-                        delete_files(filename_path , (err) => {
-                            if(err){
+
+                        delete_files(filename_path, (err) => {
+                            if (err) {
                                 return console.log(err);
                             }
 
