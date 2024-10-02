a50da04bdc01c8b60eba426214486f5b85e52ff3
Ashutosh-98765 <asshu65656@gmail.com>
2024-10-02 18:22:07 +0530

    refactored the function in problems file

--- FS_CALLBACK/problem1.js
+++ FS_CALLBACK/problem1.js
@@ -2,61 +2,61 @@ const fs = require('fs');
 const path = require('path');
 const directory_path = __dirname;
 // console.log(__dirname);
-const directory_name = path.join(directory_path , 'randomJsonFiles');
+const directory_name = path.join(directory_path, 'randomJsonFiles');
 // 1. Create a directory.
 const create_directory = (callback) => {
-    
-    fs.mkdir(directory_name , {recursive : true} , (err , msg) => {
-        if(err){
+
+    fs.mkdir(directory_name, { recursive: true }, (err) => {
+        if (err) {
             callback(err);
         }
-        else{
-            callback(null ,"Directory created successfully");
+        else {
+            callback(null, "Directory created successfully");
         }
     });
 }
 
 // 2. Create random JSON files
-const create_files = (no_of_files , callback) => {
+const create_files = (no_of_files, callback) => {
     let count = 0;
-    for(let i = 0 ; i < no_of_files ; i++){
-        const file_path = path.join(directory_name, `file${i+1}.json`);
-        fs.writeFile(file_path,`This is file${i+1}.`, (err , msg) => {
-            if(err){
+    for (let i = 0; i < no_of_files; i++) {
+        const file_path = path.join(directory_name, `file${i + 1}.json`);
+        fs.writeFile(file_path, `This is file${i + 1}.`, (err) => {
+            if (err) {
                 callback(err);
             }
             count++;
-            if(count === no_of_files){
-                callback(null ,"Files created successfully");
+            if (count === no_of_files) {
+                callback(null, "Files created successfully");
             }
         })
     }
 }
 
-// //  3. Delete those files simultaneously 
+//   3. Delete those files simultaneously 
 const delete_files = (callback) => {
     let count = 0;
-    fs.readdir(directory_name , (err , files) => {
-        if(err){
+    fs.readdir(directory_name, (err, files) => {
+        if (err) {
             callback(err);
         }
-        if(files.length === 0){
+        if (files.length === 0) {
             callback(err);
         }
-                
+
         files.forEach((file) => {
-            let file_path = path.join(directory_name , file);
-            fs.unlink(file_path , (err) => {
-                if(err){
+            let file_path = path.join(directory_name, file);
+            fs.unlink(file_path, (err) => {
+                if (err) {
                     callback(err);
                 }
                 count++;
-                if(count == files.length){
-                    callback(null , "Files Deleted Successfully");
+                if (count == files.length) {
+                    callback(null, "Files Deleted Successfully");
                 }
             })
         });
     });
 }
 
-module.exports = {create_directory , create_files , delete_files};
+module.exports = { create_directory, create_files, delete_files };
